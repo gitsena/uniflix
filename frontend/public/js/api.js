9b129f9b2b47142d5f971e44f0ff27cd5d3cfd20
Andre Sena <andrediasdesena@gmail.com>
2025-05-24 15:16:57 -0300

    deploy

--- frontend/public/js/api.js
+++ frontend/public/js/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000"; // ajuste se necessário
+//const API_URL = "http://localhost:3000"; // ajuste se necessário
+const API_URL = "https://uniflix-api.onrender.com"; // por exemplo
 
 export async function fetchFilmes(filtros = {}) {
   const queryParams = new URLSearchParams();
